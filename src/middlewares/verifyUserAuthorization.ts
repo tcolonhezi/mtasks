3ab9a283c9769ad5e339d6898ae9b1e14d877daf
Thiago Colonhezi <thiago.colonhezi@gmail.com>
2026-08-24 08:33:14 -0300

    refactor: update verifyAuthorization middleware to use Role type and simplify permission check; rename sessionsRouter to sessionsRoutes for consistency

--- src/middlewares/verifyUserAuthorization.ts
+++ src/middlewares/verifyUserAuthorization.ts
@@ -1,15 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "./error-handling.js";
+import { Role } from "../generated/prisma/enums.js";
 
-function verifyAuthorization(requiredPermissions: string[]) {
+function verifyAuthorization(requiredPermissions: Role[]) {
   return (request: Request, response: Response, next: NextFunction) => {
     if (!request.user) {
       throw new AppError("User not authenticated.", 401);
     }
 
-    const hasPermission = requiredPermissions.some((permission) =>
-      request.user.role.includes(permission),
-    );
+    const hasPermission = requiredPermissions.includes(request.user.role);
 
     if (!hasPermission) {
       throw new AppError("User does not have the required role.", 403);
--- src/routes/sessions-routes.ts
+++ src/routes/sessions-routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { SessionController } from "../controllers/sessions-controller.js";
 
-const sessionsRouter = Router();
+const sessionsRoutes = Router();
 const sessionsController = new SessionController();
 
-sessionsRouter.post("/", sessionsController.create);
+sessionsRoutes.post("/", sessionsController.create);
 
-export { sessionsRouter };
+export { sessionsRoutes as sessionsRouter };
